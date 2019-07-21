788f8ef3b646d04d87ac13336e158a183431462e
Ajia  Oluwatobi <aoluwatobi@teamapt.com>
2019-07-21 18:23:33 +0100

    First Commit

---
+++ src/main/java/com/eltobeski/invoicingapp/InvoicingappApplication.java
@@ -0,0 +1,13 @@
+package com.eltobeski.invoicingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicingappApplication.class, args);
+	}
+
+}
